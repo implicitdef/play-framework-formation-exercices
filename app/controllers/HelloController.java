c3428c9507128c07ccee9a4da224ca1046b2765b
implicitdef <implicitdef@users.noreply.github.com>
2017-01-30 12:38:48 +0100

    exo B

--- app/controllers/HelloController.java
+++ app/controllers/HelloController.java
@@ -7,6 +7,9 @@ import play.mvc.Result;
 public class HelloController extends Controller {
 
     public Result hello(String name, boolean uppercase) {
+        if (name.length() > 20){
+            return badRequest("Nom trop long, pas bonjour !");
+        }
         String content = "Bonjour " + name + " !";
         return ok(uppercase ? content.toUpperCase() : content);
     }

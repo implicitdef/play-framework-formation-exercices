da21dc7a7f58bd008d83f7d100e9aa89bc3cb68e
implicitdef <implicitdef@users.noreply.github.com>
2017-01-30 13:57:02 +0100

    exo J

--- app/controllers/HelloController.java
+++ app/controllers/HelloController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import play.Logger;
+import play.libs.ws.WSClient;
 import play.mvc.Controller;
 import play.mvc.Result;
 import services.HelloService;
@@ -10,16 +12,27 @@ import javax.inject.Inject;
 public class HelloController extends Controller {
 
     private HelloService helloService;
+    private WSClient wsClient;
 
     @Inject
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, WSClient wsclient) {
         this.helloService = helloService;
+        this.wsClient = wsclient;
     }
 
     public Result hello(String name, boolean uppercase) {
         if (name.length() > 20){
             return badRequest("Nom trop long, pas bonjour !");
         }
+
+        wsClient.url("https://jsonplaceholder.typicode.com/users/5").get().thenApply((response) -> {
+            if (response.getStatus() >= 200 && response.getStatus() < 300) {
+                String nameFromJson = response.asJson().get("name").asText();
+                Logger.info("Name from json : " + nameFromJson);
+            }
+            throw new RuntimeException("Bad response");
+        });
+
         String message = helloService.buildMessage(name, uppercase);
         return ok(views.html.hello.render(message));
     }
--- build.sbt
+++ build.sbt
@@ -8,3 +8,4 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava)
 scalaVersion := "2.11.8"
 
 libraryDependencies += filters
+libraryDependencies += javaWs

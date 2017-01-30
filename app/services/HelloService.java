5a6dc8a2c010d1609717b6072777868684aec252
implicitdef <implicitdef@users.noreply.github.com>
2017-01-30 13:16:05 +0100

    Exo H

--- app/services/HelloService.java
+++ app/services/HelloService.java
@@ -1,9 +1,24 @@
 package services;
 
+import play.Configuration;
+
+import javax.inject.Inject;
+
 public class HelloService {
 
+
+    private Configuration conf;
+
+    @Inject
+    public HelloService(Configuration conf) {
+        this.conf = conf;
+    }
+
     public String buildMessage(String name, Boolean uppercase){
-        String message = "Bonjour " + name + " !";
+        String message = "Bonjour " + name + " ";
+        for (int i = 0; i < conf.getInt("hello.exclamationPoints"); i++){
+            message += "!";
+        }
         return uppercase ? message.toUpperCase() : message;
     }
 
